814643f7d3ddb780bc3579e232c2e8f4d8c51a6f
한석우 <delugeofcode@gmail.com>
2025-02-23 17:07:17 +0900

    red green

--- test_money.js
+++ test_money.js
@@ -15,6 +15,15 @@ class Money {
     }
 }
 
+class Portfolio {
+    add(money) {
+    }
+    
+    evaluate(currency) {
+        return new Money(15, "USD");
+    }
+}
+
 let fiveDollars = new Money(5, "USD");
 let tenDollars = new Money(10, "USD");
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
@@ -27,3 +36,9 @@ let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
