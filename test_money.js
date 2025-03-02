f7518e32c8cbdb77ca8c8ec4d90cb1d7d7b08d6d
한석우 <delugeofcode@gmail.com>
2025-03-02 17:52:01 +0900

    remove duplicate code

--- test_money.js
+++ test_money.js
@@ -4,10 +4,6 @@ import { Portfolio } from './portfolio.js';
 
 console.log('Running tests...');
 
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
-
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
@@ -17,6 +13,9 @@ let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
 
+
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
 let fifteenDollars = new Money(15, "USD");
 let portfolio = new Portfolio();
 
