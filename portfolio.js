7ccefabee93227b88542a222ce479825b808156e
한석우 <delugeofcode@gmail.com>
2025-03-05 13:01:38 +0900

    기능별 분업업

--- portfolio.js
+++ portfolio.js
@@ -9,34 +9,22 @@ export class Portfolio {
         this.money = this.money.concat(money);
     }
     
-    evaluate(currency) {
+    evaluate(bank, currency) {
         let failures = [];
         let total = this.money.reduce( (sum, money) => {
-            let convertedAmount = this.convert(money, currency);
-            if (convertedAmount === undefined) {
-                failures.push(money.currency + "->" + currency);
+            try {
+                let convertedMoney = bank.convert(money, currency);
+                return sum + convertedMoney.amount;
+            }
+            catch (e) {
+                failures.push(e.message);
                 return sum;
             }
-            return sum + convertedAmount;
         }, 0);
+
         if (!failures.length) {
             return new Money(total, currency);
         }
         throw new Error("Missing exchange rate(s):[" + failures.join(", ") + "]");
-    }
-    
-    convert(money, currency) {
-        let exchangeRates = new Map();
-        exchangeRates.set("EUR->USD", 1.2);
-        exchangeRates.set("USD->KRW", 1100);
-        if (money.currency == currency) {
-            return money.amount;
-        }
-        let key = money.currency + "->" + currency;
-        let rate = exchangeRates.get(key);
-        if (rate === undefined) {
-            return undefined;
-        }
-        return money.amount * rate;
     }
 }
